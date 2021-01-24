8e5594462706f732e530d77ae695b74fa47c892e
amitby98 <77382577+amitby98@users.noreply.github.com>
2021-01-24 16:21:19 +0200

    added lists and list items

---
+++ main.js
@@ -0,0 +1,20 @@
+const ul = document.querySelector("ul");
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+
+function addItem() {
+  const item = input.value;
+  input.value = "";
+  const liEl = document.createElement("li");
+  const spanEl = document.createElement("span");
+  const buttonEl = document.createElement("button");
+  liEl.appendChild(spanEl);
+  liEl.appendChild(buttonEl);
+  spanEl.textContent = item;
+  buttonEl.textContent = "Delete";
+  ul.appendChild(liEl);
+
+  buttonEl.onclick = function removeItem() {
+    ul.removeChild(liEl);
+  };
+}
